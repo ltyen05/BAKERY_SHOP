c6e6fe4308200967825b12b32b90d19b897e9f94
Nguyễn Bảo Thạch <nguyenbaothach@MacBook-Pro-cua.local>
2025-11-25 00:56:58 +0700

    Update 25/11

--- models/customer.py
+++ models/customer.py
@@ -10,3 +10,9 @@ class Customer(db.Model):
     avatar = db.Column(db.String(500))
     password = db.Column(db.String(200))
     created_at = db.Column(db.DateTime)
+
+    def __init__(self, username, email, phone, password):
+        self.username = username
+        self.name = username
+        self.email = email.lower()
+        self.set_password(password)

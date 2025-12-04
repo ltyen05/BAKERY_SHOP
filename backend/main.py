6c9abeec09f8c107d267ea6db36a3c672b5309fe
Le Thi Yen <23001963.hus.edu.vn>
2025-12-04 08:45:24 +0700

    Fix work tree frontend-admin

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-from hus_bakery_app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- frontend-user/src/Facilities/Facilities.jsx
+++
@@ -1,3 +0,0 @@
-export default function Facilities() {
-  return <div>Cơ sở</div>;
-}

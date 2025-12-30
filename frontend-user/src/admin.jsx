c2e373d4f8ddda2703d149f65531758077316c50
Le Thi Yen <23001963.hus.edu.vn>
2025-12-30 10:26:41 +0700

    Merge frontend user

--- frontend-user/src/admin.jsx
+++
@@ -1,4 +0,0 @@
-function Admin() {
-  return <h1>Admin Page - Restricted Access</h1>;
-}
-export default Admin;
--- frontend-user/src/pages/user/Menu/Cake.jsx
+++
@@ -1,7 +0,0 @@
-export default function Cake() {
-  return (
-    <div>
-      <h1>Cake</h1>
-    </div>
-  );
-}

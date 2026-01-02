f6e74e4b0ccb72531e823f30b272e0ccf3fabc96
Nguyễn Bảo Thạch <nguyenbaothach@MacBook-Pro-cua.local>
2026-01-02 23:11:39 +0700

    product_management_services

--- backend/hus_bakery_app/services/admin/product_management_services.py
+++ backend/hus_bakery_app/services/admin/product_management_services.py
@@ -1,6 +1,7 @@
 from sqlalchemy import desc
 from hus_bakery_app import db
 from hus_bakery_app.models.products import Product
+from hus_bakery_app.services.customer.product_services import get_rating_star_service
 
 def get_all_products_admin_service():
     # Sắp xếp giảm dần theo ngày tạo để hiện sản phẩm mới nhất lên đầu
@@ -12,7 +13,7 @@ def get_all_products_admin_service():
         "category": p.category_id,  # Hoặc join bảng Category để lấy tên
         "unit_price": float(p.unit_price) if p.unit_price is not None else 0.0,
         "description": p.description,
-        "rating": getattr(p, 'rating', 5.0),  # Giả sử mặc định 5.0
+        "rating": get_rating_star_service(p.product_id),  # Giả sử mặc định 5.0
         "image": p.image_url
     } for p in products]
 

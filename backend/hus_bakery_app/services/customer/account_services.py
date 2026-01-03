ae55e30a09d0ae35f3e30cdd63a2b13fc227e43f
Nguyễn Bảo Thạch <nguyenbaothach@MacBook-Pro-cua.local>
2026-01-03 08:27:52 +0700

    update rating branch

--- backend/hus_bakery_app/services/customer/account_services.py
+++ backend/hus_bakery_app/services/customer/account_services.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash
 from hus_bakery_app import db
 from hus_bakery_app.models.branches import Branch
 from hus_bakery_app.models.employee import Employee
+from hus_bakery_app.models.feedback import Feedback
 from hus_bakery_app.services.customer.product_services import get_rating_star_service
 from hus_bakery_app.models.customer import Customer
 from hus_bakery_app.models.order import Order
@@ -215,6 +216,11 @@ def get_product_was_bought(customer_id):
 
     return res
 
+def get_rating_star_branch_service(branch_id):
+    average = db.session.query(func.avg(Feedback.rating))\
+        .filter(Feedback.branch_id == branch_id).scalar()
+    return round(float(average), 1) if average else 0
+
 def get_branch_detail():
     branches = Branch.query.all()
     branches_list = []
@@ -230,6 +236,7 @@ def get_branch_detail():
             "mapSrc": branch.mapSrc,
             "lat": branch.lat,
             "lon": branch.lng,
+            "rating": get_rating_star_branch_service(branch.branch_id)
         }
         branches_list.append(details)
 

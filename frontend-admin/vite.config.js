7aac0987a9ef1ab64f5c2561a6991b49c361393f
Le Thi Yen <23001963.hus.edu.vn>
2026-01-04 06:53:09 +0530

    Chuyen cong cho admin

--- frontend-admin/vite.config.js
+++ frontend-admin/vite.config.js
@@ -3,16 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // 1. Giúp load file JS/CSS từ đường dẫn /admin/
   base: "/admin/", 
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0', // Cho phép kết nối từ ngoài vào container
     port: 5173,
-    // 2. GIẢI QUYẾT LỖI "Blocked request": Cho phép tên miền của bạn đi qua
-    allowedHosts: [
-      'husbakery.duckdns.org'
-    ],
-    // 3. Giúp Hot Reload hoạt động mượt qua Nginx Proxy Manager
+    strictPort: true,
+    // THÊM DÒNG NÀY ĐỂ HẾT LỖI FORBIDDEN
+    allowedHosts: ['husbakery.duckdns.org'], 
     hmr: {
       clientPort: 443 
     }

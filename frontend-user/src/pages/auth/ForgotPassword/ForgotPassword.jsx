ecaa4fa762ab3776115a2e67fd385ab165d54869
NguyenTienLuong <nguyentienluong2005@gmail.com>
2026-01-04 09:42:34 +0700

    Update ForgotPassword.jsx
    Updated API URL to use environment variable for flexibility.

--- frontend-user/src/pages/auth/ForgotPassword/ForgotPassword.jsx
+++ frontend-user/src/pages/auth/ForgotPassword/ForgotPassword.jsx
@@ -8,7 +8,7 @@ export default function ForgotPassword() {
   const [messageApi, contextHolder] = message.useMessage();
   const handleSendEmail = async (values) => {
     try {
-      const res = await fetch("http://localhost:5000/api/forgot-password", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/forgot-password`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -76,3 +76,4 @@ export default function ForgotPassword() {
     </>
   );
 }
+
